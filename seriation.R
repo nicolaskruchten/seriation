1e055c29fdc532392caa447c6ea58e6c588e9452
Nicolas Kruchten <nicolas@datacratic.com>
2018-02-04 23:13:24 -0500

    margins

--- seriation.R
+++ seriation.R
@@ -19,17 +19,17 @@ png("shuffled_cols.png", width=outsize, height=outsize, units = 'px')
 pimage(bitmap[,shuffler], col=color, key=FALSE)
 dev.off()
 png("shuffled_both.png", width=outsize, height=outsize, units = 'px')
-pimage(shuffled, col=color, key=FALSE)
+pimage(shuffled, col=color, key=FALSE,margins=c(0,0))
 dev.off()
 
 
 png("clustered.png", width=outsize, height=outsize, units = 'px')
 heatmap.2(shuffled, trace='none', col=color, key=FALSE, labRow = FALSE, labCol = FALSE,
-          hclustfun = function(q) hclust(q, method="average") )
+          hclustfun = function(q) hclust(q, method="average"),margins=c(0,0) )
 dev.off()
 
 png("clustered_olo.png", width=outsize, height=outsize, units = 'px')
-hmap(shuffled, method="OLO_average",col=color, key=FALSE,labRow = FALSE, labCol = FALSE)
+hmap(shuffled, method="OLO_average",col=color, key=FALSE,labRow = FALSE, labCol = FALSE,margins=c(0,0))
 dev.off()
 
 png("tsp.png", width=outsize, height=outsize, units = 'px')
