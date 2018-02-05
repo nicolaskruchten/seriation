937b8ffed2a75c549697ddd67792d51f2cee9a3a
Nicolas Kruchten <nicolas@datacratic.com>
2018-02-04 22:35:21 -0500

    initial commit

---
+++ seriation.R
@@ -0,0 +1,38 @@
+library(readr)
+library(seriation)
+library(gplots)
+library(viridis)
+
+x <- read_csv("x.csv",col_names = FALSE)
+color = inferno(200)
+bitmap = as.matrix(x)
+shuffler = sample.int(nrow(bitmap))
+shuffled = bitmap[shuffler,shuffler]
+outsize = 800
+png("original.png", width=outsize, height=outsize, units = 'px')
+pimage(bitmap, col=color, key=FALSE)
+dev.off()
+png("shuffled_rows.png", width=outsize, height=outsize, units = 'px')
+pimage(bitmap[shuffler,], col=color, key=FALSE)
+dev.off()
+png("shuffled_cols.png", width=outsize, height=outsize, units = 'px')
+pimage(bitmap[,shuffler], col=color, key=FALSE)
+dev.off()
+png("shuffled_both.png", width=outsize, height=outsize, units = 'px')
+pimage(shuffled, col=color, key=FALSE)
+dev.off()
+
+
+png("clustered.png", width=outsize, height=outsize, units = 'px')
+heatmap.2(shuffled, trace='none', col=color, key=FALSE, labRow = FALSE, labCol = FALSE,
+          hclustfun = function(q) hclust(q, method="average") )
+dev.off()
+
+png("clustered_olo.png", width=outsize, height=outsize, units = 'px')
+hmap(shuffled, method="OLO_average",col=color, key=FALSE,labRow = FALSE, labCol = FALSE)
+dev.off()
+
+png("tsp.png", width=outsize, height=outsize, units = 'px')
+hmap(shuffled, method="TSP",col=color, key=FALSE,labRow = FALSE, labCol = FALSE)
+dev.off()
+
